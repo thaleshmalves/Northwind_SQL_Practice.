4ef2ca00cd3e0f9aaac88200b833ac7f0268a0f5
thaleshmalves <102166665+thaleshmalves@users.noreply.github.com>
2025-10-28 17:54:11 -0300

    Update mais_vendidos_unid.sql

--- queries/mais_vendidos_unid.sql
+++ queries/mais_vendidos_unid.sql
@@ -1,11 +1,20 @@
---Produtos mais vendidos por unidades
-
-SELECT p.product_name, SUM(od.quantity) AS total_vendas
-FROM order_details od
-JOIN products p ON od.product_id = p.product_id
-  -- teste
-GROUP BY p.product_name
-ORDER BY total_vendas DESC;
-
---Produtos mais vendidos por receita
-
+/* ==========================================================
+   QUERY:      top_selling_products.sql
+   AUTOR:      Thales Alves
+   DESCRIÇÃO:  Identifica os clientes que mais compram em volume no banco Northwind.
+   DATA:       2025-04-25
+   BASE:       northwind.public
+     ========================================================== */
+SELECT 
+  p.product_name, 
+  SUM(od.quantity) AS quantidade_total_vendida,
+  SUM(od.unit_price * od.quantity * (1 - od.discount)) AS receita_total
+FROM 
+  order_details od
+JOIN 
+  products p ON od.product_id = p.product_id
+GROUP BY 
+  p.product_name
+ORDER BY 
+  quantidade_total_vendida DESC
+  LIMIT 10;
